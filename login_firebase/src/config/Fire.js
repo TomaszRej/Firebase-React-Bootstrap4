51e63662550b4a71f3749929343160068d952ed3
Tomasz Rej <tomaszrej@gmail.com>
2018-09-21 21:13:06 +0200

    switch to github

--- login_firebase/src/config/Fire.js
+++ login_firebase/src/config/Fire.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDxgitc8yd9XlsAmaaE-TK5kqkiSHcRhUE",
-  authDomain: "myfirstproject-60f1f.firebaseapp.com",
-  databaseURL: "https://myfirstproject-60f1f.firebaseio.com",
-  projectId: "myfirstproject-60f1f",
-  storageBucket: "myfirstproject-60f1f.appspot.com",
-  messagingSenderId: "708256386610"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 const fire = firebase.initializeApp(config);
